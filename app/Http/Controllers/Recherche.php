8f0bf2aa02086a6695b7e18f84aa06ba350827d6
bastienhatinguais <bastien.hatinguais@gmail.com>
2021-12-03 02:12:56 +0100

    bd updated

--- app/Http/Controllers/Recherche.php
+++ app/Http/Controllers/Recherche.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bateau;
+use App\Models\Civil;
 use App\Models\Marin;
 use Illuminate\Http\Request;
 
@@ -9,8 +11,15 @@ class Recherche extends Controller
 {
     function recherche($recherche)
     {
-        $marins = Marin::all();
-        $marins = Marin::where('nom', 'like', '%' . $recherche . '%')->get();
+        $marins = Marin::where('nom', 'like', '%' . $recherche . '%')->orWhere('prenom', 'like', '%' . $recherche . '%')->get();
+        $bateaux = Bateau::where('bateaus.nom', 'like', '%' . $recherche . '%')
+            ->join('equipages', 'equipages.id', '=', 'bateaus.id')
+            ->join('marins', 'marins.id', '=', 'equipages.id')
+            ->join('marin_sauvetage_sauve', 'marin_sauvetage_sauve.id', '=', 'marins.id')
+            ->join('sauvetages', 'sauvetages.id', '=', 'marin_sauvetage_sauve.id')
+            ->get();
+        $civils = Civil::where('nom', 'like', '%' . $recherche . '%')->orWhere('prenom', 'like', '%' . $recherche . '%')->get();
+
         return $marins->toJson();
     }
 }
