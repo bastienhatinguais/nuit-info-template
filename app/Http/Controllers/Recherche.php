71ec7c06d405dcf035c20f4d2543a020d5b2a9e7
bastienhatinguais <bastien.hatinguais@gmail.com>
2021-12-03 04:23:05 +0100

    API bateau

--- app/Http/Controllers/Recherche.php
+++ app/Http/Controllers/Recherche.php
@@ -3,29 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bateau;
-use App\Models\Civil;
-use App\Models\Marin;
-use App\Models\Sauvetage;
+
 use Illuminate\Http\Request;
 
 class Recherche extends Controller
 {
     function recherche($recherche)
     {
-        $marins = Marin::where('nom', 'like', '%' . $recherche . '%')
-            ->orWhere('prenom', 'like', '%' . $recherche . '%');
 
-        $bateaux = Sauvetage::join('marin_sauvetage_sauve', 'sauvetage_id', '=', 'sauvetages.id')
-            ->join('marins', 'marins.id', '=', 'marin_id')
-            ->join('equipages', 'marin_id', '=', 'marins.id')
-            ->join('bateaus', 'bateaus.id', '=', 'bateau_id')
-            ->where('bateaus.nom', 'like', '%' . $recherche . '%');
+        $bateaux = Bateau::where('nom', 'like', '%' . $recherche . '%')->get();
 
-        $civils = Sauvetage::join('civil_sauvetage', 'sauvetage_id', '=', 'sauvetages.id')
-            ->join('civils', 'civils.id', '=', 'civil_id')
-            ->where('nom', 'like', '%' . $recherche . '%')
-            ->orWhere('prenom', 'like', '%' . $recherche . '%')
-            ->union($bateaux);
-        return $marins->union($civils)->get()->toJson();
+        return $bateaux->toJson();
     }
 }
